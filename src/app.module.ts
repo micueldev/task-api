7e1eb993002e47b5e096725b4a532bf3f2eddb11
Miguel Cueva <miguel.cueva.l@uni.pe>
2024-11-02 21:05:23 -0500

    feat: dabase configuration and docker-compose file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { ConfigModule as AppConfigModule } from './config/config.module';
 import { TasksModule } from './modules/tasks/infrastructure/tasks.module';
 
 @Module({
-  imports: [TasksModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    AppConfigModule,
+    TasksModule
+  ],
 })
 export class AppModule {}
